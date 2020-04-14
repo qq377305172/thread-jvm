46d06615e8a280972a20ba9790082d4ba4c5ef53
[chaojing] <[chaojing1@aliyun.com]>
2020-04-14 18:27:49 +0800

    1

--- src/main/java/jing/chao/thread/ProdConsumerBlockQueueDemo.java
+++ src/main/java/jing/chao/thread/ProdConsumerBlockQueueDemo.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -16,6 +17,17 @@ class MyResource {
     private AtomicInteger atomicInteger = new AtomicInteger();
     private BlockingQueue<String> blockingQueue = null;
 
+    public void myProd() throws InterruptedException {
+        String data = null;
+        boolean retValue;
+        while (flag) {
+            data = atomicInteger.incrementAndGet() + "";
+            retValue = blockingQueue.offer(data, 2L, TimeUnit.SECONDS);
+            if(retValue){
+
+            }
+        }
+    }
 }
 
 public class ProdConsumerBlockQueueDemo {
